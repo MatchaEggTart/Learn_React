d0a5f1deeccb39e84e5e1bac344e4456277bd497
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-02-19 02:33:54 +0800

    update

---
+++ add_react_to_html/container.js
@@ -0,0 +1,23 @@
+/*
+function c() {
+  const element = (
+    <div>
+    <h1>Hello, World!</h1>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  // ReactDOM.render(element, document.getElementById('react-container'));
+  const container = ReactDOM.createRoot(document.getElementById('react-container'));
+  container.render(element);
+}
+
+setInterval(c, 1000);
+*/
+
+const element = (
+  <div>
+  <h1>Hello, World!</h1>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById('react-container')).render(element);
