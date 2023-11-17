6ccd9d727b872ba5eb3699266e152cf6cce14147
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-11-17 22:26:09 +0800

    update

--- learn-react-app/src/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-function HelloWorld() {
-  return (
-    <div className="HelloWorld">
-      <h1>Hello World!</h1>
-    </div>
-  )
-}
-
-export default HelloWorld;
--- learn-react-app/src/Welcome.js
+++
@@ -1,9 +0,0 @@
-function Welcome(props) {
-  return (
-    <div className="Welcome">
-      <h1>Welcome, {props.name} !</h1>
-    </div>
-  )
-}
-
-export default Welcome;
